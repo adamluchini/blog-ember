bee3d90c935985ffc9d3cc22edcbcd305523cdd2
Adam Luchini and Bhawani Parajuli <adam@>
2016-08-17 13:29:23 -0700

    add update component.

---
+++ app/components/update-post.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    update(post) {
+      var params = {
+        title: this.get('title'),
+        body: this.get('body'),
+        image: this.get('image')? this.get('image'): "",
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
+});
