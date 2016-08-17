1172faef7b2176707911d0fcbd0ef014375ed18c
Adam Luchini and Bhawani Parajuli <bhawani@>
2016-08-17 10:14:57 -0700

    add a form to post new blog.

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  newPostForm: false,
+  actions: {
+    newPostFormShow() {
+    this.set('newPostForm', true);
+  },
+  save1() {
+    var params = {
+      title: this.get('title'),
+      body: this.get('body'),
+      image: this.get('image'),
+    };
+    this.set('newPostForm', false);
+    this.sendAction('save2', params);
+    }
+  }
 });
